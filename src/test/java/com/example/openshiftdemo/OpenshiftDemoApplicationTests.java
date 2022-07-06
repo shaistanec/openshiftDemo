271587ef987331b9f35c23fa57e61e51e81edad5
shaista.shafi <onr@kmd.dk>
2022-07-06 11:09:48 +0530

    Initial commit

---
+++ src/test/java/com/example/openshiftdemo/OpenshiftDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.openshiftdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenshiftDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
